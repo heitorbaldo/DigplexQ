13185718931ca464f7a546abd9fd6b872e8c233b
Heitor Baldo <heitorb@protonmail.com>
2023-11-27 12:30:20 -0300

    Add files via upload

--- digplexq/discrete_curvatures.py
+++ digplexq/discrete_curvatures.py
@@ -3,77 +3,137 @@ Discrete Curvatures: Ollivier-Ricci and Forman-Ricci Curvatures.
 '''
 
 import numpy as np
-import math as m
+import math
+
+from digplexq.digraph_based_complexes import *
 from digplexq.simplicial_weights import *
 from digplexq.utils import *
-from digplexq.digraph_based_complexes import *
+
 
 __all__ = [
-    "incident_edges_simplex",
-    "incident_edges_node",
-    "forman_ricci_curvature",
+    "incoming_edges",
+    "outgoing_edges",
+    "incoming_edges_vertex",
+    "outgoing_edges_vertex",
+    "q_forman_ricci_curvature",
+    "in_q_forman_ricci_curvature",
+    "out_q_forman_ricci_curvature",
 ]
 
 
-
-def incident_edges_simplex(DFC, simplex):
-    '''Returns the set of incident edges.
+def incoming_edges(DFC, edge_curv):
+    '''Returns the set of incoming edges.
     Parameters
+    ----------
     '''
-    inc_edges = []
-    for simp in DFC[0]:
-        for i in range(len(simplex)):
-            if simp[0] == simplex[i] or simp[1] == simplex[i]:
-                inc_edges.append(simp)
-            else:
-                pass
-    return inc_edges
-
-
-def incident_edges_node(DFC, simplex, k):
-    '''Returns the set of incident edges.
-    Parameters
-    ---------
-    DFC:
-    simplex:
-    k: node
-    '''
-    inc_edges_node = []
-    IE = incident_edges_simplex(DFC, simplex)
-    
-    for edge in IE:
-        if edge[0] == k or edge[1] == k:
-                inc_edges.append(edge)
+    in_edges = []
+    for edge in DFC[0]:
+        if edge[1] == edge_curv[0]:
+                in_edges.append(edge)
         else:
             pass
-    return inc_edges_node
+    return in_edges
 
 
-def forman_ricci_curvature(M, DFC, simplex, weight_func='max'):
-    '''Returns the Forman-Ricci curvature of a weighted simplex or weighted path.
+def outgoing_edges(DFC, edge_curv):
+    '''Returns the set of outgoing edges.
     Parameters
-    ---------
-    M: NumPy matrix.
-    simplex: NumPy array.
-    Returns
-    ---------
+    ----------
     '''
-    S1_k = S2_k = 0
-    w_simplex = simplex_weight(M, simplex, weight_func)
-    w_nodes = node_weights_simplex(M, simplex, weight_func)
-    sum_node_weights = sum(w_nodes)
-    IE = incident_edges_simplex(DFC, simplex)
+    out_edges = []
+    for edge in DFC[0]:
+        if edge[0] == edge_curv[1]:
+                out_edges.append(edge)
+        else:
+            pass
+    return out_edges
 
-    for k in range(len(simplex)):
-        S1_k += w_nodes[k]/w_simplex
-        
-    for k in range(len(simplex)):
-        IE_k = incident_edges_node(DFC, simplex, k)
-        for edge in IE_k:
-            S2_k += w_nodes[k] / m.sqrt(w_simplex*simplex_weight(M, edge, weight_func))
-    
-    FRC = w_simplex*(S1_k - S2_k)
-    return FRC
+def incoming_edges_vertex(DFC, v):
+    '''Returns the set of incoming edges.
+    Parameters
+    ----------
+    '''
+    in_edges = []
+    for edge in DFC[0]:
+        if edge[1] == v:
+                in_edges.append(edge)
+        else:
+            pass
+    return in_edges
+
+
+def outgoing_edges_vertex(DFC, v):
+    '''Returns the set of outgoing edges.
+    Parameters
+    ----------
+    '''
+    out_edges = []
+    for edge in DFC[0]:
+        if edge[0] == v:
+                out_edges.append(edge)
+        else:
+            pass
+    return out_edges
    
 
+def q_forman_ricci_curvature(M, DFC, edge_curv, weight_func='max'):
+    '''Returns the Forman-Ricci curvature of a weighted directed edge.
+    Parameters
+    ----------
+    M: NumPy matrix.
+    edge: NumPy array.
+    '''
+    S0_k = 0
+    S1_k = 0
+    S2_k = 0
+    
+    w_edge = simplex_weight(M, edge_curv, weight_func)
+    w_nodes = node_weights_simplex(M, edge_curv, weight_func)
+    
+    IE = incoming_edges(DFC, edge_curv)
+    OE = outgoing_edges(DFC, edge_curv)
+
+    for k in range(2):
+        S0_k += w_nodes[k]/w_edge
+        
+    for edge in IE:
+        S1_k += w_nodes[0] / math.sqrt(w_edge*simplex_weight(M, edge, weight_func))
+        
+    for edge in OE:
+        S2_k += w_nodes[1] / math.sqrt(w_edge*simplex_weight(M, edge, weight_func))
+    
+    FRC = w_edge*(S0_k - S1_k - S2_k)
+    return round(FRC, 5)
+   
+
+def in_q_forman_ricci_curvature(M, DFC, v, weight_func='max'):
+    '''Returns the in-q-Forman-Ricci curvature of a vertex.
+    Parameters
+    ----------
+    M: NumPy matrix.
+    v: integer.
+    '''
+    inFRC = 0
+    IE = incoming_edges_vertex(DFC, v)
+    
+    for edge in IE:    
+        inFRC += q_forman_ricci_curvature(M, DFC, edge, weight_func='max')
+    
+    return round(inFRC, 5)
+
+
+def out_q_forman_ricci_curvature(M, DFC, v, weight_func='max'):
+    '''Returns the in-q-Forman-Ricci curvature of a vertex.
+    Parameters
+    ----------
+    M: NumPy matrix.
+    v: integer.
+    '''
+    outFRC = 0
+    IO = outgoing_edges_vertex(DFC, v)
+    
+    for edge in IO:    
+        outFRC += q_forman_ricci_curvature(M, DFC, edge, weight_func='max')
+    
+    return round(outFRC, 5)
 
