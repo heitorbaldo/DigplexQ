632ae9c22a9428214b0c76c1032bf0d192b6d892
Heitor Baldo <heitorb@protonmail.com>
2023-12-05 22:58:14 -0300

    Add files via upload

--- digplexq/simplicial_neural_networks.py
+++ digplexq/simplicial_neural_networks.py
@@ -1,17 +1,6 @@
 '''
-Simplicial Neural Networks.
+TODO.
+Reference: Ebli, S. et al. (2020). Simplicial Neural Networks. arXiv:2010.03633.
 '''
 
-import numpy as np
-import tensorflow as tf
-import keras as kr
-
-import torch
-import torch.nn as nn
-import numpy as np
-import scipy.sparse as sp
-
-import scnn.chebyshev
-
-#------------  -------------
 
