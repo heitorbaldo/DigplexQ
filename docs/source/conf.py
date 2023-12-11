f78c009422ff7d7429948ecbc8a0607796c1d583
Heitor Baldo <heitorb@protonmail.com>
2023-12-11 10:32:18 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,11 @@ author = 'heitor'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    ...
+    'sphinx_rtd_theme',
 ]
+
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
