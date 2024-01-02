b694065e938bd00c99a274e45281364c42e40b19
Heitor Baldo <heitorb@protonmail.com>
2024-01-02 16:12:46 -0300

    Add files via upload

--- digplexq/__init__.py
+++ digplexq/__init__.py
@@ -4,8 +4,8 @@ DigplexQ
 
 DigplexQ is a Python package to perform computations with digraph-based complexes.
 
-See http://www.heitorbaldo.com/digplexq for complete documentation.
+See https://github.com/heitorbaldo/DigplexQ for complete documentation.
 """
 
 #Current version:
-__version__ = "0.0.4"
+__version__ = "0.0.5"
